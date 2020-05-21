a8b69f2deab6a85762aa677d116c95f7a9889b9f
Kenneth-Brandon <onegiantsfan25@yahoo.com>
2020-05-20 19:38:44 -0700

    CRUD and MVP

--- api/server.js
+++ api/server.js
@@ -1,9 +1,14 @@
-const express = require("express");
-
-const db = require("../data/dbConfig.js");
+const express = require('express');
+const PostRouter = require('../posts/post-router.js');
+const db = require('../data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+server.use('/api/posts', PostRouter);
+
+server.get('/', (req, res) => {
+  res.status(200).json({ api: 'up' });
+});
 
 module.exports = server;
